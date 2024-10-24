14fb2f20f7b8f07207b13da077ba3df94b3d1e51
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-10-24 20:36:57 +0200

    Have added two Projects one for DataAccess and Core

---
+++ PolyglotPersistenceSolution/Core/Data/Mongo/Company.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Mongo
+{
+    public class Company
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public long serialNumber { get; set; }
+        public string Name { get; set; }
+        public string Telephone { get; set; }
+        public string Country { get; set; }
+
+    }
+}
---
+++ PolyglotPersistenceSolution/Core/Data/Mongo/ProductWithCompany.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Mongo
+{
+    internal class ProductWithCompany
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public int ProductNumber { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string ImageUrl { get; set; }
+        public Seller Sales { get; set; }
+        public Seller? Available {  get; set; }
+        public ICollection<Seller> Distributes { get; set; }
+    }
+}
