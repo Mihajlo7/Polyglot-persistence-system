8fd04622d035d97ab01642b406e6bef090ab7a9b
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-10-28 17:29:57 +0100

    Have created project for perfomance measure

---
+++ PolyglotPersistenceSolution/PerfomanceMeasure/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace PerfomanceMeasure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<TestBench>();
+        }
+    }
+}
---
+++ PolyglotPersistenceSolution/PerfomanceMeasure/TestBench.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+using SQLDataAccess.impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfomanceMeasure
+{
+    [SimpleJob(launchCount: 1,warmupCount:2,iterationCount:6)]
+    public class TestBench
+    {
+        private readonly ConsumerRepository consumerRepository=new();
+        [Benchmark]
+        public async Task Test()
+        {
+            await consumerRepository.GetAllBySelect();
+        }
+    }
+}
