a15d39bae80b2e4f6fdde8308478c50bb870b300
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-10-28 17:40:15 +0100

    Have created models for Company

---
+++ PolyglotPersistenceSolution/Core/Models/CompanyModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class CompanyModel
+    {
+        public Guid Id { get; set; }
+        public string DunsNumber { get; set; }
+        public string Name { get; set; }
+        public string Telephone {  get; set; }
+        public string Country { get; set; }
+    }
+}
