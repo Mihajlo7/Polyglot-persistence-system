317fc86f81db4ae603784ed9e7e50553c08665e5
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-10-28 18:09:41 +0100

    Have added model class for Products

---
+++ PolyglotPersistenceSolution/Core/ExternalData/ProductAndDetailsEx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Core.ExternalData
+{
+    public class ProductAndDetailsEx
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        [JsonPropertyName("short_description")]
+        public string ShortDescription { get; set; }
+        [JsonPropertyName("image_url")]
+        public string ImageUrl { get; set; }
+        [JsonPropertyName("sub_category")]
+        public string SubCategory { get; set; }
+        
+    }
+}
---
+++ PolyglotPersistenceSolution/Core/Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class ProductModel
+    {
+        public Guid Id { get; set; }
+        public long ProductNumber { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public SellerModel Produced { get; set; }
+        public SellerModel? Store {  get; set; }
+        public ICollection<SellerModel> Distribute {  get; set; }
+        public SubCategoryModel SubCategory { get; set; }
+        public ProductDetailsModel Details { get; set; }
+    }
+}
