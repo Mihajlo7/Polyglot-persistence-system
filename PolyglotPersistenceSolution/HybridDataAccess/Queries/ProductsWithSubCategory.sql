0cffec44d7f3d86e952a9d55fb6ac25c3205c938
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-11-13 18:42:15 +0100

    Have added queries for product with details

---
+++ PolyglotPersistenceSolution/HybridDataAccess/Queries/ProductsWithSubCategory.sql
@@ -0,0 +1,42 @@
+﻿
+-- Get all products --
+SELECT product.id Id,product.name Name,product.price Price, s.id SubCategoryId, s.name SubCategoryName 
+FROM SubCategories s
+CROSS APPLY OPENJSON(products)
+WITH (
+	id BIGINT '$.id',
+	name NVARCHAR(30) '$.name',
+	price DECIMAL(5,2) '$.price'
+	)AS product;
+
+-- Get product by Id --
+SELECT product.id Id,product.name Name,product.price Price, s.id SubCategoryId, s.name SubCategoryName 
+FROM SubCategories s
+CROSS APPLY OPENJSON(products)
+WITH (
+	id BIGINT '$.id',
+	name NVARCHAR(30) '$.name',
+	price DECIMAL(5,2) '$.price'
+	)AS product
+WHERE product.id=@ProductId;
+
+-- Get products By SubCategoryId --
+SELECT products
+FROM SubCategories
+WHERE id=@SubCategoryId;
+
+-- Get products By SubCategoryName
+SELECT products
+FROM SubCategories
+WHERE name=@SubCategoryName;
+
+-- Update price by ProductId --
+WITH cte AS(
+	SELECT *
+	FROM SubCategories CROSS APPLY OPENJSON(products)
+)
+UPDATE cte
+SET products = JSON_MODIFY(products,'$['+cte.[key]+'].Price',@Price)
+
+-- Update price by SubCategoryId
+
