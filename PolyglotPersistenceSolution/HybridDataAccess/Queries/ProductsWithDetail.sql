a7b066771864e5bff24e6406b4a64d98fa398889
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-11-14 10:41:35 +0100

    Have added queries for products with detail

---
+++ PolyglotPersistenceSolution/HybridDataAccess/Queries/ProductsWithDetail.sql
@@ -0,0 +1,41 @@
+﻿CREATE TABLE Products(
+	id BIGINT CONSTRAINT product_d_pk PRIMARY KEY(id),
+	name NVARCHAR(50) NOT NULL,
+	price DECIMAL(7,2) NOT NULL,
+	detail NVARCHAR(MAX) NULL,
+	subCategoryId BIGINT
+)
+GO
+
+CREATE OR ALTER PROC CreateProductWithDetail
+	@Id BIGINT,
+	@Name NVARCHAR(50),
+	@Price DECIMAL(7,2),
+	@Detail NVARCHAR(MAX),
+	@SubCategoryId BIGINT
+AS
+BEGIN
+	INSERT INTO Products (id,name,price,detail,subCategoryId) VALUES (@Id,@Name,@Price,@Detail,@SubCategoryId)
+END
+GO
+
+DROP TYPE IF EXISTS ProductWithDetailType;
+GO
+
+CREATE TYPE ProductWithDetailType AS TABLE(
+	Id BIGINT,
+	Name NVARCHAR(50),
+	Price DECIMAL(7,2),
+	Detail NVARCHAR(MAX),
+	SubCategoryId BIGINT
+);
+GO
+
+CREATE OR ALTER PROC CreateProductsWithDetailsBulk
+	@ProductsWithDetails ProductWithDetailType READONLY
+AS
+BEGIN
+	INSERT INTO Products (id,name,price,detail,subCategoryId)
+	SELECT Id, Name,Price,Detail,SubCategoryId
+	FROM @ProductsWithDetails
+END;
